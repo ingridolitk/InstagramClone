08dcf0da430727742d26f1437f61ad6691ae4d4a
ingridolitk <ingrid.oliveiratk@gmail.com>
2022-11-24 22:40:07 -0300

    Initial commit

---
+++ app/src/main/java/com/ingrid/instagram/activity/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.ingrid.instagram.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ingrid.instagram.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
